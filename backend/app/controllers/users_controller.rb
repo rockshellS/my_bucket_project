5e7d70ae834728dda545a42583b89c32b49bfd86
rockshellS <spicer.rachelle@gmail.com>
2021-06-21 14:19:47 -0400

    sign-up updaeted

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -4,24 +4,44 @@ class UsersController < ApplicationController
         render json: users
       end
 
-      def create 
-     
-        user = User.find_by(userName: params[:userName])
+    def user_auth
+      
+      user = User.find_by(userName: params[:userName])
 
-        if user && user.authenticate(params[:password])
+      if user && user.authenticate(params[:password])
+        payload ={ user_id: user.id }
+        hmac_secret = 'S3CR3T'
+
+        token = JWT.encode(payload, hmac_secret, 'HS256')
+
+            render json: { user: UserSerializer.new(user), token:token }
+          else
+          render json: {error: 'Invaild name or password'}
+         
+        end  
+    end
+
+
+    def create 
+     
+        user = User.new(user_params)
+        if user.save
+        
           payload ={ user_id: user.id }
           hmac_secret = 'S3CR3T'
 
           token = JWT.encode(payload, hmac_secret, 'HS256')
 
-              render json: { user: UserSerializer.new(user), token:token }
-            else
-            render json: {error: 'Invaild name or password'}
+          render json: { user: UserSerializer.new(user), token:token }
+          else
+          render json: {error: 'Invaild name or password'}
+            
           end  
        end
 
    
       def show
+        
         token = request.headers[:Authorization].split(' ')[1]
         begin
           decoded_token = JWT.decode(token, 'S3CR3T', true, { algorithm: 'HS256' })
@@ -33,6 +53,11 @@ class UsersController < ApplicationController
         end
       end
 
-  
+      private 
+
+      def user_params
+        params.require(:user).permit(:userName, :city, :password)
+      end
+
 
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   # post '/users', to 'users#create'
   get '/current_user', to: 'users#show'
+  post '/user_auth', to: 'users#user_auth'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
